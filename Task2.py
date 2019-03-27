f517b2cd1d7bcb930ba7eb9ad238b90aba2d99d8
Jayden Zangari <jayden.zangari@gmail.com>
2019-03-27 20:59:58 +1100

    Progress on task3

--- Task2.py
+++ Task2.py
@@ -3,15 +3,15 @@ import sys
 
 class Bed:
     """
-    Class representing beds. Stores its name and length along with its
+    Class representing beds. Stores its name, length and width along with its
     compatibility.
     """
 
-    def __init__(self, name, x, y):
+    def __init__(self, name, x, y, compatibility):
         self.name = name
         self.x = x
         self.y = y
-        self.persons_compatible = 0
+        self.persons_compatible = compatibility
 
     def get_name(self):
         return self.name
@@ -37,7 +37,7 @@ class Bed:
 
 class Person:
     """
-    Class representing beds. Stores their name and minimum length
+    Class representing beds. Stores their name and minimum length and width
     for compatibility with a bed.
     """
 
@@ -90,8 +90,8 @@ def read_file(filename):
         # Create a list of first set of Bed objects storing their
         # x,y coordinates and names
         beds_set1 = lines[size_persons1:size_persons1+size_beds1]
-        beds_set1 = list(map((lambda x: (Person(x.split()[0], float(x.split()[1]), float(x.split()[2])))),
-                                beds_set1))
+        beds_set1 = list(map((lambda x: (Bed(x.split()[0], float(x.split()[1]), float(x.split()[2]), int(x.split()[3])
+                                             ))), beds_set1))
 
         # Create a list of second set of Person objects storing their
         # x,y coordinates and names
@@ -102,15 +102,72 @@ def read_file(filename):
         # Create a list of second set of Bed objects storing their
         # x,y coordinates and names
         beds_set2 = lines[size_persons1+size_beds1+size_persons2:size_persons1+size_beds1+size_persons2+size_beds2]
-        beds_set2 = list(map((lambda x: (Person(x.split()[0], float(x.split()[1]), float(x.split()[2])))),
-                             beds_set2))
+        beds_set2 = list(map((lambda x: (Bed(x.split()[0], float(x.split()[1]), float(x.split()[2]), int(x.split()[3])
+                                             ))), beds_set2))
+
 
     return persons_set1, beds_set1, persons_set2, beds_set2
+
+
+def merge(left_persons, left_beds, right_persons, right_beds):
+    """
+    Merges the two sets together by calculating the compatibility of each bed in right_beds with each person in
+    left_persons.
+    :param left_persons: Subset of persons left of the vertical line
+    :param left_beds: Subset of beds left of the vertical line
+    :param right_persons: Subset of persons right of the vertical line
+    :param right_beds: Subset of beds right of the vertical line
+    :return: The merged beds with updated compatibility
+    """
+
+    persons_length = len(left_persons)
+    beds_length = len(right_beds)
+
+    # Pointer keeping track of the left persons
+    persons_pointer = 0
+    # Pointer keeping track of the right beds
+    beds_pointer = 0
+
+    num_compatible = 0
+
+    while persons_pointer < persons_length and beds_pointer < beds_length:
+        person = left_persons[persons_pointer]
+        bed = right_beds[beds_pointer]
+
+        # If bed's width greater than minimum person's
+        # width. Then it is compatible with them and all of the
+        # persons that came before it. Move to the next person
+        if bed.get_y() >= person.get_y():
+            num_compatible += 1
+            persons_pointer += 1
+
+        # Else set compatibility and move to the next bed
+        else:
+            current_compatibility = bed.get_compatibility()
+            bed.set_compatibility(num_compatible + current_compatibility)
+            beds_pointer += 1
+
+    # If we ran out of left persons, set the compatibility of remaining
+    # right beds to the one before it
+    if beds_pointer < persons_pointer:
+        for bed in right_beds[beds_pointer:]:
+            bed.set_compatibility(bed.get_compatibility() + num_compatible)
+
+    # Merge two sets and return
+    left_beds.extend(right_beds)
+    return left_beds
+
+
+def print_results(beds):
+    for bed in beds:
+        print('{} {}'.format(bed.get_name(), bed.get_compatibility()))
 
 
 def __main__():
     filename = sys.argv[1]
     nodes = read_file(filename)
+    beds = merge(nodes[0], nodes[1], nodes[2], nodes[3])
+    print_results(beds)
 
 
 __main__()
--- Task3.py
+++ Task3.py
@@ -3,13 +3,14 @@ import sys
 
 class Bed:
     """
-    Class representing beds. Stores its name and length along with its
+    Class representing beds. Stores its name, length and width along with its
     compatibility.
     """
 
-    def __init__(self, name, x):
+    def __init__(self, name, x, y):
         self.name = name
         self.x = x
+        self.y = y
         self.persons_compatible = 0
 
     def get_name(self):
@@ -21,6 +22,12 @@ class Bed:
     def set_x(self, x):
         self.x = x
 
+    def get_y(self):
+        return self.y
+
+    def set_y(self, y):
+        self.y = y
+
     def set_compatibility(self, persons_compatible):
         self.persons_compatible = persons_compatible
 
@@ -30,13 +37,14 @@ class Bed:
 
 class Person:
     """
-    Class representing beds. Stores their name and minimum length
+    Class representing beds. Stores their name and minimum length and width
     for compatibility with a bed.
     """
 
-    def __init__(self, name, x):
+    def __init__(self, name, x, y):
         self.name = name
         self.x = x
+        self.y = y
 
     def get_name(self):
         return self.name
@@ -47,10 +55,16 @@ class Person:
     def set_x(self, x):
         self.x = x
 
+    def get_y(self):
+        return self.y
+
+    def set_y(self, y):
+        self.y = y
+
 
 def read_file(filename):
     """
-    Reads the x coordinate for each person and bed. Returns
+    Reads the x and y coordinate for each person and bed. Returns
     a list of Bed and Person classes containing said x coordinate.
     :param filename: Name of the file
     :return: Tuple containing lists of Beds and Persons
@@ -62,24 +76,25 @@ def read_file(filename):
         amount_persons = int(lines[0])
 
         # Create a list of Person objects storing their
-        # x coordinates and names
+        # x and ycoordinates and names
         persons = lines[1:amount_persons+1]
-        persons = list(map((lambda x: (Person(x.split()[0], float(x.split()[1])))), persons))
+        persons = list(map((lambda x: (Person(x.split()[0], float(x.split()[1]), float(x.split()[2])))), persons))
 
         # Create a list of Bed objects storing their
-        # x coordinates and names
+        # x and y coordinates and names
         beds = lines[amount_persons+1: len(lines)]
-        beds = list(map((lambda x: (Bed(x.split()[0], float(x.split()[1])))), beds))
+        beds = list(map((lambda x: (Bed(x.split()[0], float(x.split()[1]), float(x.split()[2])))), beds))
 
     return persons, beds
 
 
-def merge(left_nodes, right_nodes):
+def merge(left_nodes, right_nodes, coordinate):
     """
     Merges two lists of nodes in ascending order
-    of their x coordinates.
+    of their x or ycoordinates.
     :param left_nodes: Left nodes to be sorted
     :param right_nodes: Right nodes to be sorted
+    :param coordinate: The coordinate (x or y) to compare the nodes with
     :return: Merged list
     """
 
@@ -100,7 +115,11 @@ def merge(left_nodes, right_nodes):
 
         # If the left node <= right node, add it to the merge list
         # and increment the left pointer.
-        if left_node.get_x() <= right_node.get_x():
+        if coordinate == 'x' and left_node.get_x() <= right_node.get_x():
+            merged_list.append(left_node)
+            left_pointer += 1
+
+        elif coordinate == 'y' and left_node.get_y() <= right_node.get_y():
             merged_list.append(left_node)
             left_pointer += 1
 
@@ -121,27 +140,33 @@ def merge(left_nodes, right_nodes):
     return merged_list
 
 
-def sort_two(node1, node2):
+def sort_two(node1, node2, coordinate):
     """
     Returns a list of the two nodes.
     If node1 <= node2, node1 is the first element.
     If node1 > node2, node2 is the second element.
+    Compares the nodes by x or y coordinate, specified by coordinate.
     :param node1: First node
     :param node2: Second node
+    :param coordinate: The coordinate (x or y) to compare the nodes with
     :return: List of sorted nodes
     """
 
-    if node1.get_x() <= node2.get_x():
+    if coordinate == 'x' and node1.get_x() <= node2.get_x():
+        return [node1, node2]
+
+    elif coordinate == 'y' and node1.get_y() <= node2.get_y():
         return [node1, node2]
 
     return [node2, node1]
 
 
-def merge_sort(nodes):
+def merge_sort(nodes, coordinate):
     """
-    Sorts nodes by ascending order of x coordinate.
+    Sorts nodes by ascending order of x or y coordinate.
     Uses recursive merge sort.
     :param nodes: Nodes to be sorted
+    :param coordinate: The x or y coordinate to sort by
     :return: List of sorted nodes
     """
 
@@ -151,80 +176,60 @@ def merge_sort(nodes):
 
     # If nodes has 2 elements, sort them
     if len(nodes) < 3:
-        return sort_two(nodes[0], nodes[1])
+        return sort_two(nodes[0], nodes[1], coordinate)
 
     # Else, divide in two and recurse
     half_point = len(nodes)//2
     left_nodes = nodes[:half_point]
     right_nodes = nodes[half_point:len(nodes)]
 
-    left_nodes = merge_sort(left_nodes)
-    right_nodes = merge_sort(right_nodes)
+    left_nodes = merge_sort(left_nodes, coordinate)
+    right_nodes = merge_sort(right_nodes, coordinate)
 
     # merge the lists and return them
-    return merge(left_nodes, right_nodes)
+    return merge(left_nodes, right_nodes, coordinate)
 
-
-def calculate_compatibility(persons, beds):
+def divide(nodes, x):
     """
-    Calculates the compatibility of each bed
-    by comparing its dimensions to each person's
-    minimal requirements. Returns None if unequal amount
-    of persons and beds.
-    :param persons: Sorted list of person objects
-    :param beds: Sorted list of bed objects
-    :return: Bed objects with updated compatibility. None if
-    beds and persons are unequal.
+    Divides a list of nodes into two sets based on a vertical line denoted by an x coordinate.
+    :param nodes: List of nodes to be divided
+    :param x: x coordinate of the vertical line
+    :return: tuple containing two divided lists
     """
 
-    if len(beds) != len(persons):
-        return None
+    left_List = list()
+    right_list = list()
 
-    # Initialise pointers to track positions in
-    # both lists
-    persons_pointer = 0
-    beds_pointer = 0
-
-    num_compatible = 0
-
-    while persons_pointer < len(persons) and beds_pointer < len(beds):
-        person = persons[persons_pointer]
-        bed = beds[beds_pointer]
-
-        # If bed's length greater than minimum person's
-        # length. Then it is compatible with them and all of the
-        # persons that came before it. Move to the next person
-        if bed.get_x() >= person.get_x():
-            num_compatible += 1
-            persons_pointer += 1
-
-        # Else set compatibility and move to the next bed
-        else:
-            bed.set_compatibility(num_compatible)
-            beds_pointer += 1
-
-    # If we ran out of persons, set the compatibility of remaining
-    # beds to the one before it
-    if beds_pointer < persons_pointer:
-        for bed in beds[beds_pointer:]:
-            bed.set_compatibility(num_compatible)
-
-    return beds
+    for node in nodes:
 
 
-def print_compatibility(beds):
-    for bed in beds:
-        print('{} {}'.format(bed.get_name(), bed.get_compatibility()))
+def divide_and_conquer(sorted_persons_y, sorted_beds_y, sorted_beds_x):
+    """
+    Recursive divide and conquer algorithm to compute the compatibility of each bed.
+    Divides the y sorted beds in half by x coordinate.
+    :param sorted_persons_y: Persons sorted by y coordinate
+    :param sorted_beds_y: Beds sorted by y coordinate
+    :param sorted_beds_x: Beds sorted by x coordinate
+    :return: sorted_beds_y with updated compatibilities
+    """
+
+    # TODO: Base case
+
+    # Split sorted_beds_x roughly in half and find the middle x coordinate
+    left_beds_x = sorted_beds_x[:len(sorted_beds_x)//2]
+    right_beds_x = sorted_beds_x[len(sorted_beds_x)//2:]
+    mix_x_coordinate = left_beds_x[-1].get_x()
+
 
 
 def __main__():
     filename = sys.argv[1]
     nodes = read_file(filename)
 
-    sorted_persons = merge_sort(nodes[0])
-    sorted_beds = merge_sort(nodes[1])
-
-    compatible_beds = calculate_compatibility(sorted_persons, sorted_beds)
-    print_compatibility(compatible_beds)
+    # Sort persons by y coordinate.
+    # Sort beds by x and y coordinate separately
+    sorted_persons_y = merge_sort(nodes[0], 'y')
+    sorted_beds_y = merge_sort(nodes[1], 'y')
+    sorted_beds_x = merge_sort(nodes[1], 'x')
 
 __main__()
